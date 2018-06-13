63620ba33e4f57a0fdf25e3e66575cf17e121ae9
Winshen <winshen@uchicago.edu>
2018-06-13 10:49:37 -0500

    refactored with coins array and iteration

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -1,31 +1,15 @@
 class Coinchanger
 
 	def dispense(change)
-		penny = 1
-		nickel = 5
-		dime = 10
-		quarter = 25
+		coins = [25, 10, 5, 1]
 		coin_array = []
 
-		while change >= 25
-			change -= 25
-			coin_array << quarter
+		coins.each do |coin|
+			while change >= coin
+				change -= coin
+				coin_array << coin
+			end
 		end
-
-		while change >= 10
-			change -= 10
-			coin_array << dime
-		end
-
-		while change >= 5
-			change -= 5
-			coin_array << nickel
-		end
-
-		change.times do |i|
-				coin_array << penny
-		end
-
 		coin_array
 	end
 end
